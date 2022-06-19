c594c5c7ab49e4c1299add1d64d486d0b7a2bd5a
Muthuraman1994 <muthubaran@gmail.com>
2022-06-19 15:15:42 +0530

    Sample1

---
+++ src/gitTask/DataEntry.java
@@ -0,0 +1,17 @@
+package gitTask;
+
+public class DataEntry {
+
+	public static void main(String[] args) {
+		
+		String S = "Welcome";
+		String S2 = " Madam ";
+		String S3 = "welcome";
+		
+		int length = S.length();
+		System.out.println(length);
+		
+		int indexOf = S.indexOf('e');
+		System.out.println(indexOf);
+	}
+}
